9cc42a335492a126dd71225652357f013371de27
Iván <salarivan97@gmail.com>
2026-05-07 13:35:20 +0200

    Cambios y termina el sevidor

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ require ('dotenv').config(); // Carga las variables del archivo .env
 const app = require ('./src/app');
 const conectarDB = require ('./src/config/database');
 
-const PUERTO
+const PUERTO = process.env.PORT || 3000;
+
+// Primero conecta la base de datos, luego arranca el servidor
+const iniciar = async () => {
+    await conectarDB();
+    app.listen(PUERTO, ()=>{
+        console.log(`Servidor corriendo en el puerto: ${PUERTO} `);
+    });
+};
+
+iniciar();
