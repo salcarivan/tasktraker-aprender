048243f42bc2fbd56b50c1367087fbcc726b573e
Iván <salarivan97@gmail.com>
2026-05-07 12:57:42 +0200

    Crea conexión a base de datos

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose =require ('moongose');
+
+const conectarDB = async () => {
+    try {
+        const conexion= await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB conectado: ${conexion.connection.host}`)
+    }catch(error){
+        console.error(`Error al conectar: ${error.message}`),
+        process.exit(1); // Apaga el servidor si no puede conectarse
+    }
+};
+
+module.exports =conectarDB;
